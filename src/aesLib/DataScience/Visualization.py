fcb569c6d479db4916b576a51b49e197df73e6f0
Arnt Erik Stene <ARNTS@equinor.com>
2020-01-02 12:23:33 +0100

    Function documentation

--- src/aesLib/DataScience/Visualization.py
+++ src/aesLib/DataScience/Visualization.py
@@ -3,7 +3,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotHeatmapBetweenColumns(df: pd.DataFrame, size: tuple, savefig: bool, pathAndFileName='heatmap') -> plt.axes:
+def plotHeatmapBetweenColumns(df: pd.DataFrame, size: tuple, savefig=False, pathAndFileName='heatmap') -> plt.axes:
+    """Plot heatmap between values of multiple columns in a pandas dataframe.
+    
+    Arguments:
+        df {pd.DataFrame} -- Pandas dataframe
+        size {tuple} -- Size of output figure
+    
+    Keyword Arguments:
+        savefig {bool} -- True to save figure (default: {False})
+        pathAndFileName {str} -- Filepath specified with filename location for image (default: {'heatmap'})
+    
+    Returns:
+        plt.axes -- Matplotlib axes object
+    """
     fig = plt.figure(figsize=size)
 
     ax = sns.heatmap(df, vmin=-1, cmap='coolwarm', annot=True)
@@ -12,3 +25,6 @@ def plotHeatmapBetweenColumns(df: pd.DataFrame, size: tuple, savefig: bool, path
         plt.savefig(pathAndFileName)
 
     return ax
+
+def exampleFunction(df: pd.DataFrame):
+    
