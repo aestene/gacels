e52b957e72d67e942ef4feaa674355c166c822ea
Arnt Erik Stene <ARNTS@equinor.com>
2020-01-03 13:47:46 +0100

    Added fixture for basic dataframe

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,7 @@ def pandasSeriesFixture():
     data = [1, 4, 6, 7, 2, 6, 80, 8, 10, 33, 789, 23, 67, 102]
     return pd.Series(data)
     
+@pytest.fixture(scope='session')
+def pandasDataFrameFixture():
+    data = {'col1': [1, 2, 3, 4], 'col2': [1, 2, 3, 4]}
+    return pd.DataFrame(data)
