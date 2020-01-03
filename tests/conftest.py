356b41cd8a0f795bbb2db89033380a1ee7245a3d
Arnt Erik Stene <ARNTS@equinor.com>
2020-01-03 09:53:24 +0100

    Pylint

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,6 @@ import pandas as pd
 
 @pytest.fixture(scope='session')
 def pandasSeriesFixture():
-    data = [1,4,6,7,2,6,80,8,10,33,789,23,67,102]
+    data = [1, 4, 6, 7, 2, 6, 80, 8, 10, 33, 789, 23, 67, 102]
     return pd.Series(data)
+    
