291df4f805c661f83e52b1df480b32c327bb249d
Arnt Erik Stene <ARNTS@equinor.com>
2020-01-03 11:07:35 +0100

    Added tests for display rows

--- tests/DataScience/TestDataFrameTools.py
+++ tests/DataScience/TestDataFrameTools.py
@@ -1,4 +1,12 @@
+import pandas as pd
+
 from src.AesLib.DataScience import DataFrameTools as dft
+
+def testSetRowsToDisplayPandas():
+    rows = 100
+    dft.setRowsToDisplayPandas(rows)
+
+    assert pd.options.display.max_rows == rows
 
 def testGetIdxmaxIntegerPosOfSeries(pandasSeriesFixture):
     maxIndex = dft.getIdxmaxIntegerPosOfSeries(pandasSeriesFixture)
