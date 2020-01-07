a55e4441afeaf682bfe5e225fa80877f40d9482d
Arnt Erik Stene <ARNTS@equinor.com>
2020-01-07 14:29:27 +0100

    Tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,10 @@ def pandasSeriesFixture():
     data = [1, 4, 6, 7, 2, 6, 80, 8, 10, 33, 789, 23, 67, 102]
     return pd.Series(data)
     
+@pytest.fixture(scope='session')
+def keyVaultName():
+    return 'arnts-keyvault'
+
+@pytest.fixture(scope='session')
+def secretName():
+    return 'secret-for-testing'
