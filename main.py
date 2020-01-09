439e4f992924b5dea6be32afbb67ca910d098d9b
Arnt Erik Stene <ARNTS@equinor.com>
2020-01-09 09:07:50 +0100

    Removed __init__ and caches

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.AesLib.DataEngineering.Bazefield import downloadDataFromBazefieldAsCSV
 
 if __name__ == '__main__':
 
-    fromTimeStamp = dt.datetime(year=2018, month=1, day=21)
+    fromTimeStamp = dt.datetime(year=2018, month=2, day=16)
     toTimeStamp = dt.datetime(year=2018, month=11, day=1)
 
     downloadDataFromBazefieldAsCSV(fromTimeStamp, toTimeStamp)
