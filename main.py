fffff2dd11640967eb3dceee01a74e16efdf03eb
Arnt Erik Stene <arnts@equinor.com>
2020-01-10 13:25:45 +0100

    Package pypi

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import os
 import datetime as dt
 from src.AesLib.DataEngineering.Bazefield import downloadDataFromBazefieldAsCSV
+from src.AesLib.DataScience import DataFrameTools
 
-if __name__ == '__main__':
-
+def downloadData():
     fromTimeStamp = dt.datetime(year=2018, month=2, day=16)
     toTimeStamp = dt.datetime(year=2018, month=11, day=1)
 
     downloadDataFromBazefieldAsCSV(fromTimeStamp, toTimeStamp)
+
+def processDataFrames():
+    csvFiles = os.listdir('data')
+    csvFiles = ['data/' + filename for filename in csvFiles]
+    DataFrameTools.stackCsvFiles(csvFiles)
+
+if __name__ == '__main__':
+    processDataFrames()
