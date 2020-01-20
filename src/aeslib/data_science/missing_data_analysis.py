cba34d1ae8fadfbc37995ac63d18f6ba612ec43e
Arnt Erik Stene <arnts@equinor.com>
2020-01-20 13:30:54 +0100

    Docstring

--- src/aeslib/data_science/missing_data_analysis.py
+++ src/aeslib/data_science/missing_data_analysis.py
@@ -13,6 +13,23 @@ def plot_correlation_between_missing_data(df: pd.DataFrame):
         df {pd.DataFrame} -- Pandas dataframe.
     """
     msno.heatmap(df)
+
+def plot_missing_intervals(df: pd.DataFrame, group_by=None):
+    """Plot missing intervals using functionality from the missingno package.
+    
+    Arguments:
+        df {pd.DataFrame} -- Dataframe to plot missing data for.
+    
+    Keyword Arguments:
+        group_by {str} -- Specify the name of a column in df to groupby if desired. 
+        Otherwise leave it as none. (default: {None})
+    """
+    if group_by is not None:
+        grouped = df.groupby(group_by)
+        for _, group in grouped:
+            msno.matrix(group)
+    else:
+        msno.matrix(df)
     
 def find_missing_intervals_for_dataframe(df: pd.DataFrame, cols: List = None) -> pd.DataFrame:
     """Returns a dataframe with all intervals where one or more columns contain a NaN value.
@@ -29,14 +46,3 @@ def find_missing_intervals_for_dataframe(df: pd.DataFrame, cols: List = None) ->
         pd.DataFrame -- Dataframe with intervals.
     """
     return ia.IntervalAnalysis.get_empty_intervals(df=df, cols=cols)
-
-def plot_missing_intervals(df: pd.DataFrame, group_by=None):
-    if group_by is not None:
-        grouped = df.groupby(group_by)
-        for _, group in grouped:
-            msno.matrix(group)
-    else:
-        msno.matrix(df)
-        
-def plot_missing_value_column_correlation(df: pd.DataFrame):
-    msno.heatmap(df)
