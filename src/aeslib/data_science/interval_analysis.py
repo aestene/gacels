553253efdad184d0725923bd5ecb63bb884b7555
Arnt Erik Stene <arnts@equinor.com>
2020-01-20 12:05:53 +0100

    Linting

--- src/aeslib/data_science/interval_analysis.py
+++ src/aeslib/data_science/interval_analysis.py
@@ -214,3 +214,4 @@ class Utility:
     def mean_absolute_percentage_error(y_true, y_pred):
         y_true, y_pred = np.array(y_true), np.array(y_pred)
         return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+        
