758e0516b22fd7dd1eebc0f2162a08a9dfabd36a
Arnt Erik Stene <arnts@equinor.com>
2020-01-21 12:57:26 +0100

    Refactoring of data_corrector

--- src/aeslib/data_science/interval_analysis.py
+++ src/aeslib/data_science/interval_analysis.py
@@ -4,10 +4,9 @@ import random
 
 import pandas as pd
 import numpy as np
-
-
-
 import matplotlib.pyplot as plt
+
+from aeslib.data_science.utility import Utility
 
 class IntervalAnalysis:
     def __init__(self, df_interval: pd.DataFrame):
@@ -200,22 +199,24 @@ class IntervalAnalysisMulti(IntervalAnalysis):
     #   Returns a list of MultiIndex levels preceeding DatetimeIndex level
     #
     @staticmethod
-    def _get_slice_levels(df: pd.DataFrame) -> List:
-        assert type(df.index) is pd.MultiIndex, "Use IntervalAnalysis class for single-indexed data frames"
+    def get_slice_levels(df: pd.DataFrame) -> List:
+        assert isinstance(df.index, pd.MultiIndex),\
+             "Use IntervalAnalysis class for single-indexed data frames"
         slice_levels = []
 
         for level in df.index.levels:
-            if type(level) is pd.DatetimeIndex:
+            if  isinstance(level, pd.DatetimeIndex):
                 break
             slice_levels.append(level.name)
         return slice_levels
 
     @staticmethod
-    def _get_timeindex_name(df: pd.DataFrame) -> str:
-        assert type(df.index) is pd.MultiIndex, "Use IntervalAnalysis class for single-indexed data frames"
+    def get_timeindex_name(df: pd.DataFrame) -> str:
+        assert isinstance(df.index, pd.MultiIndex),\
+            "Use IntervalAnalysis class for single-indexed data frames"
 
         for level in df.index.levels:
-            if type(level) is pd.DatetimeIndex:
+            if isinstance(level, pd.DatetimeIndex):
                 return level.name
         return None
 
@@ -223,7 +224,7 @@ class IntervalAnalysisMulti(IntervalAnalysis):
     @staticmethod
     def iterate_time_levels(df: pd.DataFrame):
 
-        slice_levels = IntervalAnalysisMulti._get_slice_levels(df)
+        slice_levels = IntervalAnalysisMulti.get_slice_levels(df)
 
         for slicer in df.index.to_frame().reset_index(drop=True).groupby(slice_levels).size().index:
             yield slicer, df.loc[slicer]
@@ -232,20 +233,21 @@ class IntervalAnalysisMulti(IntervalAnalysis):
     @classmethod
     def get_empty_intervals(cls, df: pd.DataFrame, cols: List = None)-> pd.DataFrame:
         
-        slice_levels = IntervalAnalysisMulti._get_slice_levels(df)
-        timeindex_name = IntervalAnalysisMulti._get_timeindex_name(df)
+        slice_levels = IntervalAnalysisMulti.get_slice_levels(df)
+        timeindex_name = IntervalAnalysisMulti.get_timeindex_name(df)
         dfres = None
 
         for slicer, dfslice in IntervalAnalysisMulti.iterate_time_levels(df):
             df_interval = super(IntervalAnalysisMulti, cls).get_empty_intervals(dfslice, cols)
             df_interval.reset_index(inplace=True)
-            df_interval.rename(columns={'index':timeindex_name},inplace=True)
-            if len(slice_levels )==1: slicer = [slicer]
+            df_interval.rename(columns={'index':timeindex_name}, inplace=True)
+            if len(slice_levels) == 1:
+                slicer = [slicer]
         
             for idx, level in enumerate(slice_levels):
                 df_interval[level] = slicer[idx]
 
-            dfres = df_interval if dfres is None else pd.concat([dfres,df_interval], axis=0)
+            dfres = df_interval if dfres is None else pd.concat([dfres, df_interval], axis=0)
         dfres.set_index(slice_levels + [timeindex_name], inplace=True)
         return dfres
 
@@ -254,48 +256,28 @@ class IntervalAnalysisMulti(IntervalAnalysis):
         raise NotImplementedError
 
     @staticmethod
-    def plot_distribution(df_interval: pd.DataFrame, cut_durations: dt.timedelta = None, figsize:tuple = (16,16), bins=50):
+    def plot_distribution(df_interval: pd.DataFrame,
+                          cut_durations: dt.timedelta = None,
+                          figsize: tuple = (16, 16),
+                          bins=50):
         raise NotImplementedError
 
     @staticmethod
-    def plot_threshold_experiment(df_errs: pd.DataFrame, method:str, figsize:tuple = (16,16)):
+    def plot_threshold_experiment(df_errs: pd.DataFrame, method: str, figsize: tuple = (16, 16)):
         raise NotImplementedError
 
     @staticmethod
-    def plot_timeline(df_interval: pd.DataFrame, df: pd.DataFrame, figsize:tuple = (16,16)):
+    def plot_timeline(df_interval: pd.DataFrame, df: pd.DataFrame, figsize: tuple = (16, 16)):
         raise NotImplementedError
 
     @staticmethod
-    def threshold_experiment(df: pd.DataFrame, df_interval: pd.DataFrame, method:str, missing_lengths: List, \
-                cols: List = None,
-                big_enough: dt.timedelta = dt.timedelta(days=10),\
-                nmissing:int = 15, \
-                min_neighbours:int =1 )->pd.DataFrame:
+    def threshold_experiment(df: pd.DataFrame,
+                             df_interval: pd.DataFrame,
+                             method: str,
+                             missing_lengths: List,
+                             cols: List = None,
+                             big_enough: dt.timedelta = dt.timedelta(days=10),
+                             nmissing: int = 15,
+                             min_neighbours: int = 1) -> pd.DataFrame:
         raise NotImplementedError
-
-
-class Utility:
-
-    @staticmethod
-    def get_freq(df: pd.DataFrame)->dt.timedelta:
-        assert len(df > 2), "Not enough data in the data set"
-
-        check_level = None
-
-        #Compensate for MultiIndex - find Datetime level
-        if isinstance(df.index, pd.MultiIndex):
-            for level in df.index.levels:
-                if isinstance(level, pd.DatetimeIndex):
-                    check_level = level
-                    break
-        elif isinstance(df.index, pd.DatetimeIndex):
-            check_level = df.index
-
-        assert check_level is not None, "Cannot define frequency - no DatetimeIndex given"
-        return check_level[1]-check_level[0]
-
-    @staticmethod
-    def mean_absolute_percentage_error(y_true, y_pred):
-        y_true, y_pred = np.array(y_true), np.array(y_pred)
-        return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
         
