efb26cee96d28f05252d0fc92bd9868892d69dc6
Arnt Erik Stene <arnts@equinor.com>
2020-01-21 08:20:09 +0100

    Multiindex interval analysis

--- src/aeslib/data_science/interval_analysis.py
+++ src/aeslib/data_science/interval_analysis.py
@@ -194,6 +194,85 @@ class IntervalAnalysis:
         plt.suptitle(f"{method} Interpolation error (MAPE) by missing gap size")
         plt.show()
 
+class IntervalAnalysisMulti(IntervalAnalysis):
+
+    #
+    #   Returns a list of MultiIndex levels preceeding DatetimeIndex level
+    #
+    @staticmethod
+    def _get_slice_levels(df: pd.DataFrame) -> List:
+        assert type(df.index) is pd.MultiIndex, "Use IntervalAnalysis class for single-indexed data frames"
+        slice_levels = []
+
+        for level in df.index.levels:
+            if type(level) is pd.DatetimeIndex:
+                break
+            slice_levels.append(level.name)
+        return slice_levels
+
+    @staticmethod
+    def _get_timeindex_name(df: pd.DataFrame) -> str:
+        assert type(df.index) is pd.MultiIndex, "Use IntervalAnalysis class for single-indexed data frames"
+
+        for level in df.index.levels:
+            if type(level) is pd.DatetimeIndex:
+                return level.name
+        return None
+
+
+    @staticmethod
+    def iterate_time_levels(df: pd.DataFrame):
+
+        slice_levels = IntervalAnalysisMulti._get_slice_levels(df)
+
+        for slicer in df.index.to_frame().reset_index(drop=True).groupby(slice_levels).size().index:
+            yield slicer, df.loc[slicer]
+
+
+    @classmethod
+    def get_empty_intervals(cls, df: pd.DataFrame, cols: List = None)-> pd.DataFrame:
+        
+        slice_levels = IntervalAnalysisMulti._get_slice_levels(df)
+        timeindex_name = IntervalAnalysisMulti._get_timeindex_name(df)
+        dfres = None
+
+        for slicer, dfslice in IntervalAnalysisMulti.iterate_time_levels(df):
+            df_interval = super(IntervalAnalysisMulti, cls).get_empty_intervals(dfslice, cols)
+            df_interval.reset_index(inplace=True)
+            df_interval.rename(columns={'index':timeindex_name},inplace=True)
+            if len(slice_levels )==1: slicer = [slicer]
+        
+            for idx, level in enumerate(slice_levels):
+                df_interval[level] = slicer[idx]
+
+            dfres = df_interval if dfres is None else pd.concat([dfres,df_interval], axis=0)
+        dfres.set_index(slice_levels + [timeindex_name], inplace=True)
+        return dfres
+
+    @staticmethod
+    def get_timeline(df_interval: pd.DataFrame, df: pd.DataFrame) -> pd.DataFrame:
+        raise NotImplementedError
+
+    @staticmethod
+    def plot_distribution(df_interval: pd.DataFrame, cut_durations: dt.timedelta = None, figsize:tuple = (16,16), bins=50):
+        raise NotImplementedError
+
+    @staticmethod
+    def plot_threshold_experiment(df_errs: pd.DataFrame, method:str, figsize:tuple = (16,16)):
+        raise NotImplementedError
+
+    @staticmethod
+    def plot_timeline(df_interval: pd.DataFrame, df: pd.DataFrame, figsize:tuple = (16,16)):
+        raise NotImplementedError
+
+    @staticmethod
+    def threshold_experiment(df: pd.DataFrame, df_interval: pd.DataFrame, method:str, missing_lengths: List, \
+                cols: List = None,
+                big_enough: dt.timedelta = dt.timedelta(days=10),\
+                nmissing:int = 15, \
+                min_neighbours:int =1 )->pd.DataFrame:
+        raise NotImplementedError
+
 
 class Utility:
 
