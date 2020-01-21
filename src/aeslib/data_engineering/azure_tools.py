76e0eb4fc57d985276400cf17dfa42669232eb34
Arnt Erik Stene <arnts@equinor.com>
2020-01-21 10:38:41 +0100

    Changed to class layout

--- src/aeslib/data_engineering/azure_tools.py
+++ src/aeslib/data_engineering/azure_tools.py
@@ -2,15 +2,16 @@ from azure.keyvault.secrets import SecretClient
 from azure.common.client_factory import get_client_from_cli_profile
 from azure.mgmt.compute import ComputeManagementClient
 
-def get_secret_from_key_vault(key_vault_name: str, secret_name: str):
-    key_vault_uri = "https://" + key_vault_name + ".vault.azure.net"
-    credential = get_client_from_cli_profile(ComputeManagementClient).config.credentials
-    client = SecretClient(key_vault_uri, credential)
+class AzureKeyvaultHelper:
+    def __init__(self, key_vault_name: str):
+        self.key_vault_uri = "https://" + key_vault_name + ".vault.azure.net"
 
-    return client.get_secret(secret_name).value
+    def get_secret(self, secret_name: str):
+        credential = get_client_from_cli_profile(ComputeManagementClient).config.credentials
+        client = SecretClient(self.key_vault_uri, credential)
+        return client.get_secret(secret_name).value
 
-def set_secret_in_key_vault(key_vault_name: str, secret_name: str, secret_value):
-    key_vault_uri = "https://" + key_vault_name + ".vault.azure.net"
-    credential = get_client_from_cli_profile(ComputeManagementClient).config.credentials
-    client = SecretClient(key_vault_uri, credential)
-    client.set_secret(secret_name, secret_value)
+    def set_secret(self, secret_name: str, secret_value):
+        credential = get_client_from_cli_profile(ComputeManagementClient).config.credentials
+        client = SecretClient(self.key_vault_uri, credential)
+        client.set_secret(secret_name, secret_value)
--- src/main.py
+++ src/main.py
@@ -7,21 +7,31 @@ import pandas as pd
 from aeslib.data_science import dataframe_tools as dft
 from aeslib.data_science import interval_analysis as ia
 from aeslib.data_engineering import bazefield as bf
+from aeslib.data_engineering import azure_tools as at
 
 def download_data():
-    from_timestamp = dt.datetime(year=2017, month=11, day=1)
-    to_timestamp = dt.datetime(year=2018, month=11, day=1)
+    from_timestamp = dt.datetime(year=2017, month=10, day=1)
+    to_timestamp = dt.datetime(year=2017, month=10, day=1)
     interval = '10'
     aggregates = 'Min,Max,Average,End,Standard deviation'
-    key_vault_name = 'arnts-keyvault'
+    keyvault = at.AzureKeyvaultHelper(key_vault_name='arnts-keyvault')
+
     turbines_re_string = 'DOW-[a-zA-Z_]\d{2}-(StateRun|ActivePower|ActivePowerLimit|ReactivePower|NacelleDirection|WindSpeed|OilLevel|ActualWindDirection_mean|AmbientTemp|BladeAngle|BladeAngleRef|Forecast-Available)(U|V|W|$|A|B|C)'
     calc_re_string = 'DOW-[a-zA-Z_]\d{2}-CALC-(TheoreticalProduction)($)'
     meteorological_re_string = 'DOW-F000-Met-THP-(AirTemp|AirHumidity)$'
     weather_forecast_re_string = 'DOW-EFS-(WindSpeed|WindDir|WaveDir|CurrentSpeed|CurrentDir)($|-10m|-40m|-110m)'
 
     reg_ex_strings = [turbines_re_string, calc_re_string, meteorological_re_string, weather_forecast_re_string]
-
-    bf.download_data_from_bazefield_as_csv(from_timestamp, to_timestamp, aggregates, interval, reg_ex_strings, key_vault_name)
+    
+    destination = '../Data/'
+    bazefield_client = bf.BazefieldClient(keyvault=keyvault,
+                                          from_timestamp=from_timestamp,
+                                          to_timestamp=to_timestamp,
+                                          reg_ex_strings=reg_ex_strings,
+                                          aggregates=aggregates,
+                                          interval=interval,
+                                          destination=destination)
+    bazefield_client.download_data_from_bazefield_as_csv()
 
 def process_data_frames():
     csv_files = os.listdir('data/')
