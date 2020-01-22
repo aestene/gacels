513ca4f0190fab1ff2ec8d2640dfecdcb39edb3a
Arnt Erik Stene <arnts@equinor.com>
2020-01-22 08:59:08 +0100

    Bump version

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aestene-lib", # Replace with your own username
-    version="0.1.2",
+    version="0.1.3",
     author="Arnt Erik Stene",
     author_email="steneae@gmail.com",
     description="Package containing reusable tools for different applications.",
