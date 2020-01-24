0fdf4721b03a16142438883e486336d2dcec393f
Arnt Erik Stene <arnts@equinor.com>
2020-01-24 08:40:04 +0100

    Bump version

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gacels", # Replace with your own username
-    version="0.1.7",
+    version="0.1.8",
     author="Arnt Erik Stene",
     author_email="steneae@gmail.com",
     description="Package containing reusable tools for different applications.",
