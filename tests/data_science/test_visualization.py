b9ae383f5299394e931069105ce9b719e844dad8
Arnt Erik Stene <arnts@equinor.com>
2020-01-24 08:58:27 +0100

    Commented out test of deprecated function

--- tests/data_science/test_visualization.py
+++ tests/data_science/test_visualization.py
@@ -2,15 +2,15 @@ import os
 
 from src.gacels.data_science import visualization as vis
 
-def test_plot_heatmap_between_columns_save_figure(pandas_dataframe_fixture):
-    size = (10, 10)
+# def test_plot_heatmap_between_columns_save_figure(pandas_dataframe_fixture):
+#     size = (10, 10)
     
-    savefig = True
+#     savefig = True
 
-    filename = 'test-figure.png'
+#     filename = 'test-figure.png'
 
-    _ = vis.plot_heatmap_between_columns(pandas_dataframe_fixture, size, savefig, filename)
+#     _ = vis.plot_heatmap_between_columns(pandas_dataframe_fixture, size, savefig, filename)
 
-    assert os.path.isfile(filename)
+#     assert os.path.isfile(filename)
 
-    os.remove(filename)
+#     os.remove(filename)
